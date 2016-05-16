da9fff8664e0a92574c359e8b4a3963109294bde
rtv2222 <rtv2222@gmail.com>
2016-05-16 21:04:30 +0530

    Update createDocker.sh

--- src/main/createDocker.sh
+++ src/main/createDocker.sh
@@ -3,4 +3,4 @@ cd deploy
 cp ../target/myblogspot-0.0.1-SNAPSHOT.jar target
 cp ../src/main/docker/Dockerfile .
 docker build -t rtv2222/blogspot .
-docker run -i -p 8999:8999 rtv2222/blogspot
+docker run -i -p 8999:8999 rtv2222/blogspot &
