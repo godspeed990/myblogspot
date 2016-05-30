00bfb27a546756bce7a2dbba55b6d1b7ea776f81
rtv2222 <rtv2222@gmail.com>
2016-05-30 22:14:51 +0530

    Update createDocker.sh

--- src/main/createDocker.sh
+++ src/main/createDocker.sh
@@ -6,7 +6,7 @@ cp ../target/myblogspot-0.0.1-SNAPSHOT.jar target
 cp ../src/main/docker/Dockerfile .
 
 docker build -t rtv2222/blogspot . 
-if [ $? == 0 ]
+if [ $? = 0 ]
 then
    docker run -i -p 8999:8999 rtv2222/blogspot &  
 else
