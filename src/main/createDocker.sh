e215b32737f5e73a45d22919c67a4b8e1be93496
rtv2222 <rtv2222@gmail.com>
2016-05-30 21:53:00 +0530

    Update createDocker.sh

--- src/main/createDocker.sh
+++ src/main/createDocker.sh
@@ -1,5 +1,15 @@
+#!/bin/sh
+
 mkdir -p deploy/target
 cd deploy
 cp ../target/myblogspot-0.0.1-SNAPSHOT.jar target
 cp ../src/main/docker/Dockerfile .
-docker build -t rtv2222/blogspot . && docker run -i -p 8999:8999 rtv2222/blogspot & || echo "Failed to deploy and run docker instance of myblogspot"
+
+docker build -t rtv2222/blogspot . 
+if [ $? == 0 ]
+then
+   docker run -i -p 8999:8999 rtv2222/blogspot &  
+else
+   echo "Failed to deploy and run docker instance of myblogspot"
+   exit 1
+fi
